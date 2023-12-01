5d62f64edd65a79cedb4f405dd1ea0822f2fb412
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2023-12-01 09:00:55 +0800

    main client

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import ChatPage from "./pages/ChatPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { useAuth } from "./contexts/AuthContext";
+
+function App() {
+  console.log(useAuth()?.isLoggedIn);
+  return (
+    <main>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="/signin" element={<LoginPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.withCredentials = true;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
