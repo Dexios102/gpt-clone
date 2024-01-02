5f70c44e42902808b876e9ad5641af768a628a7b
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-02 09:19:05 +0800

    Chat Structure

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -1,5 +1,10 @@
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <section className="flex items-center justify-between mx-10">
+      <div className="border-2 border-gray-200 w-1/4">div1</div>
+      <div className="border-2 border-gray-200 w-full">div2</div>
+    </section>
+  );
 };
 
 export default ChatPage;
