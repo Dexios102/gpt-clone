60d4a671e782734f50a228b265a9fb9805b8c781
Dexter Dave Cajayon <119906753+Dexios102@users.noreply.github.com>
2024-01-02 21:04:19 +0800

    ChatItems update UI

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -1,33 +1,58 @@
 import ProfileImage from "../assets/profile.jpg";
 import { IoCreateOutline } from "react-icons/io5";
+import { FaTrashAlt } from "react-icons/fa";
+import { BsThreeDots } from "react-icons/bs";
 
 const ChatPage = () => {
   return (
-    <section className="flex items-center justify-between mx-10 gap-10 mt-4">
-      <div className=" w-1/4 rounded-xl px-4 py-4 bg-[#00171f] shadow-sm shadow-gray-800">
-        <div className="flex flex-row items-center gap-4 z-10 relative">
-          <div className="rounded-full shadow-lg shadow-gray-500/50">
-            <img
-              src={ProfileImage}
-              alt="profile"
-              className="object-fill rounded-full w-14"
-            />
-          </div>
-          <button
-            className="text-white bg-gradient-to-br from-purple-600 to-blue-500
+    <section className="flex justify-between mx-10 gap-10 mt-4">
+      <div className="w-1/4">
+        <div className="rounded-xl px-4 py-4 bg-[#00171f] shadow-sm shadow-gray-800 mb-4">
+          <div className="flex flex-row items-center gap-4 z-10 relative">
+            <div className="rounded-full shadow-lg shadow-gray-500/50">
+              <img
+                src={ProfileImage}
+                alt="profile"
+                className="object-fill rounded-full w-14"
+              />
+            </div>
+            <button
+              className="text-white bg-gradient-to-br from-[#dc2f02] to-[#ffba08]
              hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-800
              font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2
              w-full inline-flex items-center justify-between"
-          >
-            New Chat
-            <IoCreateOutline className="w-6 h-6 text-white" />
-          </button>
+            >
+              New Chat
+              <IoCreateOutline className="w-6 h-6 text-white" />
+            </button>
+          </div>
         </div>
-        <div className="border border-amber-500 mt-4 text-white hidden">
-          Content
+        <div className="rounded-xl px-4 py-4 bg-[#00171f] shadow-sm shadow-gray-800 mb-4">
+          <div className="inline-flex items-center justify-between w-full">
+            <span className="text-gray-400 border-l-2 border-amber-600 pl-2 text-sm">
+              Chat History
+            </span>
+            <button>
+              <FaTrashAlt className="text-red-500 text-xl" />
+            </button>
+          </div>
+          <div className="mt-4">
+            <div
+              className="inline-flex items-center justify-between w-full py-2
+            bg-[#212529] rounded-lg shadow-sm shadow-gray-900 px-4"
+            >
+              <div className="text-sm text-gray-300">
+                Example Chat History....
+              </div>
+              <button>
+                <BsThreeDots className="text-white" />
+              </button>
+            </div>
+          </div>
         </div>
+        <div className="rounded-xl px-4 py-4 bg-[#00171f] shadow-sm shadow-gray-800"></div>
       </div>
-      <div className="border border-orange-400 w-full rounded-xl">div2</div>
+      <div className="border border-orange-400 w-full rounded-xl">Chats</div>
     </section>
   );
 };
